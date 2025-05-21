0becc3aef5bf9397da24293435c629d803dfd94b
ANA26-1998 <tsinat.mulugeta@jasirifellow.org>
2025-05-21 15:39:14 +0200

    Deploy application to Netlify

--- src/App.tsx
+++ src/App.tsx
@@ -4,7 +4,6 @@ import Hero from './components/Hero';
 import Mission from './components/Mission';
 import Features from './components/Features';
 import Prototype from './components/Prototype';
-import Testimonials from './components/Testimonials';
 import SignUp from './components/SignUp';
 import Footer from './components/Footer';
 import './utils/animations.css';
